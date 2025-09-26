7765fc87f3b2a36abd3960a7b7879aadd1b3b9f0
nguyenhongquan <emtenquan012@gmail.com>
2025-09-26 21:18:53 +0700

    appointment request

---
+++ MedTime/Models/Requests/AppointmentCreate.cs
@@ -0,0 +1,13 @@
+﻿namespace MedTime.Models.Requests
+{
+    public class AppointmentCreate
+    {
+        public string? Doctorname { get; set; }
+
+        public string? Hospitalname { get; set; }
+
+        public DateTime? Appointmentdate { get; set; }
+
+        public string? Notes { get; set; }
+    }
+}
