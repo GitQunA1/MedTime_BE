86f9b2e474ddbc4d0d2576ea19bc9809affedd12
nguyenhongquan <emtenquan012@gmail.com>
2025-09-26 21:18:35 +0700

    add Automapper, config in Program.cs, MappingProfile

---
+++ MedTime/Helpers/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MedTime.Models.DTOs;
+using MedTime.Models.Entities;
+using MedTime.Models.Requests;
+
+namespace MedTime.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Convert cho DateTime
+            CreateMap<DateTime, DateTime>()
+                .ConvertUsing(src => DateTime.SpecifyKind(src, DateTimeKind.Unspecified));
+
+            // Convert cho DateTime?
+            CreateMap<DateTime?, DateTime?>()
+                .ConvertUsing(src => src.HasValue
+                    ? DateTime.SpecifyKind(src.Value, DateTimeKind.Unspecified)
+                    : (DateTime?)null);
+
+            // Các map khác
+            CreateMap<Appointment, AppointmentDto>();
+            CreateMap<AppointmentDto, Appointment>();
+            CreateMap<Appointment, AppointmentCreate>();
+            CreateMap<AppointmentCreate, Appointment>();
+            CreateMap<Appointment, AppointmentUpdate>();
+            CreateMap<AppointmentUpdate, Appointment>();
+        }
+    }
+
+}
--- MedTime/Program.cs
+++ MedTime/Program.cs
@@ -1,4 +1,10 @@
+using MedTime.Data;
+using MedTime.Helpers;
 using MedTime.Models.Enums;
+using MedTime.Repositories;
+using MedTime.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,8 +18,23 @@ NpgsqlConnection.GlobalTypeMapper.MapEnum<IntakeActionEnum>();
 NpgsqlConnection.GlobalTypeMapper.MapEnum<ConfirmedByEnum>();
 NpgsqlConnection.GlobalTypeMapper.MapEnum<CallStatusEnum>();
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<AppointmentRepo>();
+
+builder.Services.AddScoped<AppointmentService>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+
+builder.Services.AddDbContext<MedTimeDBContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+           .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
 
 var app = builder.Build();
 
@@ -24,6 +45,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
 
