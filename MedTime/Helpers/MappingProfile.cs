afaec7be3e596fe1f2ae03240757f94c44089ba5
nguyenhongquan <emtenquan012@gmail.com>
2025-10-01 23:44:30 +0700

    update mapping

--- MedTime/Helpers/MappingProfile.cs
+++ MedTime/Helpers/MappingProfile.cs
@@ -31,6 +31,51 @@ namespace MedTime.Helpers
             CreateMap<AppointmentCreate, Appointment>();
             CreateMap<Appointment, AppointmentUpdate>();
             CreateMap<AppointmentUpdate, Appointment>();
+
+            // Calllog mappings
+            CreateMap<Calllog, CalllogDto>();
+            CreateMap<CalllogDto, Calllog>();
+            CreateMap<CalllogCreate, Calllog>();
+            CreateMap<CalllogUpdate, Calllog>();
+
+            // Emergencycontact mappings
+            CreateMap<Emergencycontact, EmergencycontactDto>();
+            CreateMap<EmergencycontactDto, Emergencycontact>();
+            CreateMap<EmergencycontactCreate, Emergencycontact>();
+            CreateMap<EmergencycontactUpdate, Emergencycontact>();
+
+            // Guardianlink mappings
+            CreateMap<Guardianlink, GuardianlinkDto>();
+            CreateMap<GuardianlinkDto, Guardianlink>();
+            CreateMap<GuardianlinkCreate, Guardianlink>()
+                .ForMember(dest => dest.Createdat, opt => opt.MapFrom(src => DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Unspecified)));
+            CreateMap<GuardianlinkUpdate, Guardianlink>();
+
+            // Intakelog mappings
+            CreateMap<Intakelog, IntakelogDto>();
+            CreateMap<IntakelogDto, Intakelog>();
+            CreateMap<IntakelogCreate, Intakelog>();
+            CreateMap<IntakelogUpdate, Intakelog>();
+
+            // Medicine mappings
+            CreateMap<Medicine, MedicineDto>();
+            CreateMap<MedicineDto, Medicine>();
+            CreateMap<MedicineCreate, Medicine>();
+            CreateMap<MedicineUpdate, Medicine>();
+
+            // Prescription mappings
+            CreateMap<Prescription, PrescriptionDto>();
+            CreateMap<PrescriptionDto, Prescription>();
+            CreateMap<PrescriptionCreate, Prescription>();
+            CreateMap<PrescriptionUpdate, Prescription>();
+
+            // Prescriptionschedule mappings
+            CreateMap<Prescriptionschedule, PrescriptionscheduleDto>();
+            CreateMap<PrescriptionscheduleDto, Prescriptionschedule>();
+            CreateMap<PrescriptionscheduleCreate, Prescriptionschedule>();
+            CreateMap<PrescriptionscheduleUpdate, Prescriptionschedule>();
+
+            // User mapping
             CreateMap<User, UserDto>();
 
         }
